d25e96c016aee3cedef81fb8e68196e5c52b2ab9
Juandiegox21 <juandiegox17@gmail.com>
2022-09-03 21:15:39 -0400

    Move to ES modules

---
+++ helpers/cardsUrl.js
@@ -0,0 +1,7 @@
+export default {
+    ambassador: process.env['AMBASSADOR_CARD_URL'],
+    assassin: process.env['ASSASSIN_CARD_URL'],
+    captain: process.env['CAPTAIN_CARD_URL'],
+    contessa: process.env['CONTESSA_CARD_URL'],
+    duke: process.env['DUKE_CARD_URL'],
+}
--- index.js
+++ index.js
@@ -1,50 +1,42 @@
-const fs = require('fs');
-const {
-	REST
-} = require('@discordjs/rest');
-const {
-	Routes
-} = require('discord-api-types/v9');
-// Require the necessary discord.js classes
-const {
-	Client,
+import { REST } from "@discordjs/rest";
+import { Routes } from "discord-api-types/v9";
+import dotenv from "dotenv";
+import commands from "./commands/index.js";
+
+// Loading the token from .env file
+dotenv.config();
+
+import {
+    Client,
 	Intents,
-	Collection
-} = require('discord.js');
+	Collection,
+} from "discord.js";
 
 // Create a new client instance
 const client = new Client({
 	intents: [Intents.FLAGS.GUILDS]
 });
 
-// Loading commands from the commands folder
-const commands = [];
-const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
-
-// Loading the token from .env file
-const dotenv = require('dotenv');
-const envFILE = dotenv.config();
 const TOKEN = process.env['TOKEN'];
 
-
-// Edit your TEST_GUILD_ID here in the env file for development
-const TEST_GUILD_ID = envFILE.parsed['TEST_GUILD_ID'];
+const TEST_GUILD_ID = process.env['TEST_GUILD_ID'];
 
 console.log(TOKEN, TEST_GUILD_ID);
 
+let commandsData = [];
 
 // Creating a collection for commands in client
 client.commands = new Collection();
 
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`);
-	commands.push(command.data.toJSON());
+for (const command of commands) {
+	commandsData.push(command.data.toJSON());
 	client.commands.set(command.data.name, command);
 }
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
 	console.log('Ready!');
+
 	// Registering the commands in the client
 	const CLIENT_ID = client.user.id;
 
@@ -56,14 +48,14 @@ client.once('ready', () => {
 			if (!TEST_GUILD_ID) {
 				await rest.put(
 					Routes.applicationCommands(CLIENT_ID), {
-					body: commands
+					body: commandsData
 				});
 
 				console.log('Successfully registered application commands globally');
 			} else {
 				await rest.put(
 					Routes.applicationGuildCommands(CLIENT_ID, TEST_GUILD_ID), {
-					body: commands
+					body: commandsData
 				});
 
 				console.log('Successfully registered application commands for development guild');
