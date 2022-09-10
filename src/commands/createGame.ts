3480ff7c18efe3d6d939fcd80cf314cf4b7af812
Juandiegox21 <juandiegox17@gmail.com>
2022-09-10 15:09:29 -0400

    remove unused import

--- src/commands/createGame.ts
+++ src/commands/createGame.ts
@@ -1,7 +1,6 @@
 import { BaseCommandInteraction, Client } from "discord.js";
 import { Command } from "../command";
 import GameService from "../services/GameService";
-import { AxiosError, AxiosResponse } from "axios";
 
 export const createGame: Command = {
     name: "creategame",
